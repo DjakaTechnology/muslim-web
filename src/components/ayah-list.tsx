1bcc3fa90b7a5e41d54d9e3276782e9612e5ae90
dtechnology <djakajaya89@gmail.com>
2026-03-12 01:50:36 +0700

    fix: prevent layout shift on last-read highlight fade
    Always apply px-2, ring-2, rounded-xl with transparent ring.
    Only colors transition on last-read state change.

--- src/components/ayah-list.tsx
+++ src/components/ayah-list.tsx
@@ -18,7 +18,7 @@ function AyahCard({ ayah, arabicClass, latinClass, showVerseNumbers }: { ayah: A
   return (
     <div
       id={`ayah-${ayah.verseNumber}`}
-      className={`group py-5 transition-colors duration-700 data-[last-read=true]:rounded-xl data-[last-read=true]:bg-primary/10 data-[last-read=true]:ring-2 data-[last-read=true]:ring-primary/30 data-[last-read=true]:px-2 ${
+      className={`group rounded-xl px-2 py-5 transition-colors duration-700 ring-2 ring-transparent data-[last-read=true]:bg-primary/10 data-[last-read=true]:ring-primary/30 ${
         showVerseNumbers ? "show-verse-numbers" : ""
       }`}
     >
